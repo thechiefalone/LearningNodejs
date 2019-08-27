6d2b02939e5c0aacd297284664e0faa32bd991ae
thechiefalone <alperenozkan@gmail.com>
2019-08-27 15:38:04 +0200

    orders between someDate

--- SQL-WebShop/app.js
+++ SQL-WebShop/app.js
@@ -22,13 +22,14 @@ app.get("/customers", (req, res) => {
   sql
     // .sqlQuery("SELECT customerName from customers ORDER BY customerName ASC ")
     .sqlQuery(
-      //   "SELECT customerName from customers WHERE customerName like 'a%' "
-      "SELECT concat(contactFirstName, ' ', contactLastName, ' ', country) as customerName from customers WHERE country like 'USA' ORDER BY customerName ASC"
+      // "SELECT customerName from customers WHERE customerName like 'a%' "
+      // "SELECT concat(contactFirstName, ' ', contactLastName, ' ', country) as customerName from customers WHERE country like 'USA' ORDER BY customerName ASC"
+      "SELECT orderNumber from orders WHERE (orderDate BETWEEN '2003-02-01' AND '2003-02-28')"
     )
     .then(result => {
       let content = "";
       result.forEach(element => {
-        content += element.customerName + "<br>";
+        content += element.orderNumber + "<br>";
       });
       res.send(content);
     })
