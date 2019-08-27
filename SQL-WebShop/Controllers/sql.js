d3a0b4362160b3771ed314db17b9d88ae18e7776
thechiefalone <alperenozkan@gmail.com>
2019-08-27 14:14:27 +0200

    getting data from server

--- SQL-WebShop/Controllers/sql.js
+++ SQL-WebShop/Controllers/sql.js
@@ -1,1 +1,51 @@
-const mysql = require("mysql");
+const mySql = require("mysql");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const con = mySql.createConnection({
+  host: process.env.mysqlhost,
+  port: process.env.mysqlport,
+  user: process.env.mysqluser,
+  password: process.env.mysqlpassword,
+  database: process.env.mysqldatabase
+});
+
+function connect() {
+  const connectPromise = new Promise((resolve, reject) => {
+    if (con.state === "disconnected") {
+      con.connect(error => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve();
+        }
+      });
+    } else {
+      resolve();
+    }
+  });
+  return connectPromise;
+}
+function sqlQuery(query) {
+  const queryPromise = new Promise((resolve, reject) => {
+    connect()
+      .then(() => {
+        con.query(query, (error, result, fields) => {
+          if (error) {
+            reject();
+          } else {
+            resolve(result);
+          }
+        });
+      })
+      .catch(error => {
+        reject(error);
+      });
+  });
+  return queryPromise;
+}
+
+module.exports = {
+  connect,
+  sqlQuery
+};
--- SQL-WebShop/app.js
+++ SQL-WebShop/app.js
@@ -1,7 +1,37 @@
 const express = require("express");
-const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
 
+const sql = require("./Controllers/sql");
+const app = express();
 const port = process.env.PORT || 3000;
+
+console.log(process.env.mysqlhost);
+
+app.get("/testConnection", (req, res) => {
+  sql
+    .connect()
+    .then(() => {
+      res.send("Connected");
+    })
+    .catch(error => {
+      res.send(error);
+    });
+});
+app.get("/customers", (req, res) => {
+  sql
+    .sqlQuery("SELECT customerName from customers ORDER BY customerName ASC ")
+    .then(result => {
+      let content = "";
+      result.forEach(element => {
+        content += element.customerName + "<br>";
+      });
+      res.send(content);
+    })
+    .catch(error => {
+      res.send(error);
+    });
+});
 
 app.listen(port, () => {
   console.log(`App listening on port ${port}!`);
